c1df872bbb1846056e9edcb07ead850f4f0d8615
Youssef-Wahba <Y.HusseinWahba@gmail.com>
2023-08-22 15:51:07 +0300

    initial

---
+++ src/main/java/com/bm/concurrency/ConCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.bm.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConCurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bm/concurrency/ConCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConCurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
