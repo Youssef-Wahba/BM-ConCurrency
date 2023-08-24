9712e4a31d119ae87c7d495f9d9bd1f83fb3b615
ziad <ziadelsaadany73@gmail.com>
2023-08-24 12:54:16 +0300

    and EXCHANGE_RATE_API_BASE_URL constant

--- src/main/java/com/bm/concurrency/clients/conversionClient/Clients.java
+++ src/main/java/com/bm/concurrency/clients/conversionClient/Clients.java
@@ -1,12 +1,13 @@
 package com.bm.concurrency.clients.conversionClient;
 
+import com.bm.concurrency.utls.constants.Constants;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Map;
 
-@FeignClient(name = "convert", url = "https://v6.exchangerate-api.com/v6/ecf10bab01b34bf0de9636e1")
+@FeignClient(name = "ConCurrency", url = Constants.EXCHANGE_RATE_API_BASE_URL)
 
 public interface Clients {
 
