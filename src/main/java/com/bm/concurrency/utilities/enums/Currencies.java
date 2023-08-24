0b5353d2aa2cad63040741dd73408cd29b8be7b6
Norhan-127 <norhanhamza127@gmail.com>
2023-08-24 11:41:24 +0300

    countries code,flags and currencies constants

---
+++ src/main/java/com/bm/concurrency/utilities/enums/Currencies.java
@@ -0,0 +1,16 @@
+package com.bm.concurrency.utilities.enums;
+
+public enum Currencies {
+    USD,
+    Euro,
+    GBP,
+    AED,
+    BHD,
+    JPY,
+    KWD,
+    OMR,
+    QAR,
+    SAR,
+    EGP
+
+}
