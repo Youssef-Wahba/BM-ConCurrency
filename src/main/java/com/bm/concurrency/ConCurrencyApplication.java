e25d70ad742fc570739dd5ba69546a70c1bdcf5f
Youssef-Wahba <Y.HusseinWahba@gmail.com>
2023-08-28 08:25:11 +0300

    adding redis caching

--- src/main/java/com/bm/concurrency/ConCurrencyApplication.java
+++ src/main/java/com/bm/concurrency/ConCurrencyApplication.java
@@ -1,15 +1,13 @@
 package com.bm.concurrency;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCaching
 public class ConCurrencyApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConCurrencyApplication.class, args);
--- src/main/java/com/bm/concurrency/payload/DTOs/CompareDto.java
+++ src/main/java/com/bm/concurrency/payload/DTOs/CompareDto.java
@@ -7,21 +7,16 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class CompareDto {
-      @NotEmpty(message = "Base currency id cannot be empty")
-      @NotNull(message = "Id cannot be null")
+      @NotNull(message = "Base currency Id is mandatory")
       @Min(value = 1, message = "Id must be greater than or equal to 1")
       private Integer baseCurrencyId;
-
-      @NotEmpty(message = "Target currency ids list cannot be empty")
-      @NotNull(message = "Target currency ids list cannot be null")
+      @NotNull(message = "Target currency ids list is mandatory")
       @Size(min = 1, message = "Target currency ids list must have at least one element")
       private  List<@NotNull(message = "Id cannot be null") @Min(value = 1, message = "Id must be greater than or equal to 1") Integer> targetCurrencyIds;
-
-      @NotNull(message = "Amount cannot be null")
-      @NotEmpty(message = "Amount cannot be empty")
+      @NotBlank(message = "Amount is mandatory")
       @Min(value = 0, message = "Amount must be greater than or equal to 1")
       private double amount;
 }
