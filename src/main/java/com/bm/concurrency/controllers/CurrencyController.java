8f2b58967296b33d52c6d6929fed120baab7b663
Youssef-Wahba <Y.HusseinWahba@gmail.com>
2023-08-29 16:11:52 +0300

    swagger tags and operation descriptors and readme file changes

--- src/main/java/com/bm/concurrency/controllers/CurrencyController.java
+++ src/main/java/com/bm/concurrency/controllers/CurrencyController.java
@@ -17,18 +17,19 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*",maxAge = 3600)
 @RequiredArgsConstructor
-@Tag(name = "Currency controller" ,description = "This de")
+@Tag(name = "Currency controller" ,description = "Version 1 of currency controller")
 @Validated
 @RestController
 @RequestMapping("api/v1/currency")
 public class CurrencyController {
     private final ICurrencyService concurrencyService;
-    @Operation(summary = "")
+    @Operation(summary = "Get All Currency", description = "Returns list of currencies with their IDs, code and flag URLs")
     @GetMapping
     public ResponseEntity<CurrencyListResponse> getAllCurrencies(){
         return ResponseEntity.ok(concurrencyService.getCurrencyList());
     }
 
+    @Operation(summary = "Compare Currency", description = "Compares base currency with more than target currency by providing id of base currency and list of target currency and amount")
     @PostMapping("/compare")
     public ResponseEntity<CompareResponse> compareCurrencies(@Valid @RequestBody CompareDto request) {
         ExchangeRateResponse exchangeRateResponse = concurrencyService.getAllCurrencyRates(request.getBaseCurrencyId());
@@ -36,6 +37,7 @@ public class CurrencyController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Convert Currency", description = "Convert from base currency to target currency with specific amount")
     @GetMapping("/convert/{source}/{target}/{amount}")
     public ResponseEntity<ConversionResponse> convertCurrencies(@Valid ConvertDTO convertDTO)   {
         return ResponseEntity.ok(concurrencyService.convert(convertDTO.getSource(), convertDTO.getTarget(),convertDTO.getAmount()));
